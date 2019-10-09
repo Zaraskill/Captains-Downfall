f67d50069d4e10ec47269578990d9de81aa1a000
Zaraskill <corentin.lelong77@gmail.com>
2019-10-09 18:15:23 +0200

    Mise en place assets tests

---
+++ PlatinumProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    public GameObject balle;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        var z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        this.transform.Translate(x, 0, z);
+
+
+        if (Input.GetButtonDown("Fire3"))
+        {
+            Instantiate(balle, transform.position, transform.rotation);
+
+        }
+
+
+    }
+}
