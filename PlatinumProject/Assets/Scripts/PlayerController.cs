14221a13dd460ab2b88919bf051dc3d7396e3a9a
6meon <malaretsimeon@gmail.com>
2019-10-16 12:28:42 +0200

    Local Multiplayer

--- PlatinumProject/Assets/Scripts/PlayerController.cs
+++ PlatinumProject/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,17 @@ using Rewired;
 public class PlayerController : MonoBehaviour
 {
     public PlayerEntity entity;
+    public PlayerEntity entity2;
 
     private Player mainPlayer;
+    private Player mainPlayer2;
 
 
     // Start is called before the first frame update
     void Start()
     {
         mainPlayer = ReInput.players.GetPlayer("Player0");
+        mainPlayer2 = ReInput.players.GetPlayer("Player1");
     }
 
     // Update is called once per frame
@@ -22,9 +25,16 @@ public class PlayerController : MonoBehaviour
         float dirX = mainPlayer.GetAxis("HorizontalMove");
         float dirY = mainPlayer.GetAxis("VerticalMove");
 
+        float dirX2 = mainPlayer2.GetAxis("HorizontalMove");
+        float dirY2 = mainPlayer2.GetAxis("VerticalMove");
+
         Vector2 moveDir = new Vector2(dirX, dirY);
         moveDir.Normalize();
 
+        Vector2 moveDir2 = new Vector2(dirX2, dirY2);
+        moveDir2.Normalize();
+
         entity.Move(moveDir);
+        entity2.Move(moveDir2);
     }
 }
