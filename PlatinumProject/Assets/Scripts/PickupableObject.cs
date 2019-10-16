9ca8d52bd0eb116edbb339d311d9d7dafaae8ec7
6meon <malaretsimeon@gmail.com>
2019-10-16 17:18:39 +0200

    Add PickupObject script

---
+++ PlatinumProject/Assets/Scripts/PickupableObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupableObject : MonoBehaviour
+{
+    private Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
